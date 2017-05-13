66f26cabcb632ba1d2a4a10ce7bf0f0503e0e0d4
naroroniru <hoshipuri0902@gmail.com>
2017-05-14 00:05:40 +0900

    test

---
+++ app/src/main/java/com/naro143/droppedpoo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.naro143.droppedpoo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
