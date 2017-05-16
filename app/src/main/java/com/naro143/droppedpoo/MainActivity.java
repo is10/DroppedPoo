4575a2c170b292d797a4c4b688797e42c75fba70
tetty0217 <tettaro_01291978@ezweb.ne.jp>
2017-05-16 20:54:05 +0900

    move page

--- app/src/main/java/com/naro143/droppedpoo/MainActivity.java
+++ app/src/main/java/com/naro143/droppedpoo/MainActivity.java
@@ -1,13 +1,31 @@
 package com.naro143.droppedpoo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    //画面遷移
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_activity);
+        setContentView(R.layout.main_activity);
+
+
+        Button sendButton = (Button) findViewById(R.id.startButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/naro143/droppedpoo/SubActivity.java
@@ -0,0 +1,13 @@
+package com.naro143.droppedpoo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+ public class SubActivity extends AppCompatActivity{
+
+     @Override
+     protected void onCreate(Bundle savedInstanceState) {
+         super.onCreate(savedInstanceState);
+         setContentView(R.layout.sub_activity);
+     }
+ }
